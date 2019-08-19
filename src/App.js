132a735ab0e774b22f2bb502b574d1984be697ac
Alvin Gao <akgao@uci.edu>
2019-08-19 12:06:34 -0700

    Add template AppBar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 
-import HomeView from "./screens/HomeView";
+import HomeView from "./containers/HomeView";
+import { Toolbar, IconButton } from "@material-ui/core";
+import { AppBar } from "./components";
+import MenuIcon from "@material-ui/icons/Menu";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" render={HomeView} />
-      </Switch>
-    </BrowserRouter>
+    <Fragment>
+      <AppBar />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" render={HomeView} />
+        </Switch>
+      </BrowserRouter>
+    </Fragment>
   );
 }
 
