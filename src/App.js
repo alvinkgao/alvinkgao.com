24d70441d0eb3e90d78551678a7587f3e10dd179
Alvin Gao <akgao@uci.edu>
2019-08-29 13:33:53 -0700

    Use new font and add animation for home screen

--- src/App.js
+++ src/App.js
@@ -23,18 +23,7 @@ const reducer = (state = initialState, action) => {
 // };
 const theme = createMuiTheme({
   typography: {
-    fontFamily: [
-      "-apple-system",
-      "BlinkMacSystemFont",
-      '"Segoe UI"',
-      "Roboto",
-      '"Helvetica Neue"',
-      "Arial",
-      "sans-serif",
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"'
-    ].join(",")
+    fontFamily: "Courier"
   }
 });
 const App = () => {
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -6,17 +6,11 @@ import Links from "../components/Links";
 const Home = () => (
   <header className="App-header">
     <Link to="/AboutMe" style={{ color: "black", textDecoration: "none" }}>
-      <div className="firstname">
-        <Typography variant="h1">A</Typography>
-        <Typography variant="h1">L</Typography>
-        <Typography variant="h1">V</Typography>
-        <Typography variant="h1">I</Typography>
-        <Typography variant="h1">N</Typography>
+      <div className="typewriter1">
+        <Typography variant="h1">ALVIN</Typography>
       </div>
-      <div className="lastname">
-        <Typography variant="h1">G</Typography>
-        <Typography variant="h1">A</Typography>
-        <Typography variant="h1">O</Typography>
+      <div className="typewriter2">
+        <Typography variant="h1">GAO</Typography>
       </div>
     </Link>
     <Links />
