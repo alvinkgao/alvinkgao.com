6d8cd4c46504c2cfb782ddab11a74f7ddb51875e
Alvin Gao <akgao@uci.edu>
2019-08-29 16:09:48 -0700

    change animation and add favicon

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ const reducer = (state = initialState, action) => {
 // };
 const theme = createMuiTheme({
   typography: {
-    fontFamily: "Courier"
+    fontFamily: "Raleway"
   }
 });
 const App = () => {
